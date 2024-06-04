193645d29a8b57b629d1559a11798564fc38700a
Aayroon <aaronitez4@gmail.com>
2024-06-04 11:38:03 +0300

    File Upload and Retrieval

--- src/main/java/com/file/filehandling/FileHandlingApplication.java
+++ src/main/java/com/file/filehandling/FileHandlingApplication.java
@@ -1,10 +1,40 @@
 package com.file.filehandling;
 
+import com.file.filehandling.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@RestController
 @SpringBootApplication
 public class FileHandlingApplication {
+
+    @Autowired
+    private FileService fileService;
+
+
+    @PostMapping("/file")
+    public ResponseEntity<?> uploadImage(@RequestParam("file")MultipartFile file) throws IOException {
+        String uploadFile = fileService.uploadFile(file);
+
+        return ResponseEntity.status(HttpStatus.OK).body(uploadFile);
+    }
+
+    @GetMapping("/file/{fileName}")
+    public ResponseEntity<?> downloadImage(@PathVariable String fileName) {
+        byte[] fileData = fileService.downloadFile(fileName);
+
+        return ResponseEntity.status(HttpStatus.OK).
+                contentType(MediaType.valueOf("image/png")).body(fileData);
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(FileHandlingApplication.class, args);
--- src/main/java/com/file/filehandling/service/FileService.java
+++ src/main/java/com/file/filehandling/service/FileService.java
@@ -1,8 +1,14 @@
 package com.file.filehandling.service;
 
+import com.file.filehandling.entity.FileData;
 import com.file.filehandling.repository.FileRepository;
+import com.file.filehandling.util.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Optional;
 
 @Service
 public class FileService {
@@ -10,4 +16,20 @@ public class FileService {
     @Autowired
     private FileRepository fileRepository;
 
+    public String uploadFile(MultipartFile file) throws IOException {
+        FileData fileData = fileRepository.save(FileData.builder().name(file.getOriginalFilename()).
+                type(file.getContentType()).fileData(FileUtils.compressFile(file.getBytes())).
+                build());
+
+        if(fileData != null) {
+            return "File uploaded successfully: " + file.getOriginalFilename();
+        }
+        return null;
+    }
+
+    public byte[] downloadFile(String fileName) {
+        Optional<FileData> dbFileData =  fileRepository.findByName(fileName);
+        byte[] files = FileUtils.decompressFile(dbFileData.get().getFileData());
+        return files;
+    }
 }
